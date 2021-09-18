d39ef4e0ce3bf0c0159511d7771874e8e9e448f3
Chaeduk <kkj2551@naver.com>
2021-09-18 21:11:58 +0900

    create Token

--- server/schemas/user.js
+++ server/schemas/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
 const { Schema } = mongoose;
 const userSchema = new Schema({
@@ -16,5 +17,34 @@ const userSchema = new Schema({
     required: true,
   },
 });
+
+userSchema.methods.generateRefreshToken = async function () {
+  const user = this;
+  const refreshToken = await jwt.sign(
+    {
+      id: user.id,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "2w",
+    }
+  );
+  return refreshToken;
+};
+
+userSchema.methods.generateAcessToken = async function () {
+  const user = this;
+  const acessToken = await jwt.sign(
+    {
+      id: user.id,
+      nickname: user.nickname,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1h",
+    }
+  );
+  return acessToken;
+};
 
 module.exports = mongoose.model("User", userSchema);
