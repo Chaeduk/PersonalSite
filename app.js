18a686031bbc2db5902b529402dbf12de11b2d46
Chaeduk <kkj2551@naver.com>
2021-09-24 09:16:11 +0900

    add react build file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const path = require("path");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const authRouter = require("./routes/auth");
@@ -12,12 +13,17 @@ connect();
 app.set("port", 4000);
 
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "client/build")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
 app.use("/api/auth", authRouter);
 app.use("/api/post", postRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
 app.listen(app.get("port"), () => {
   console.log(app.get("port"), "번 포트에서 대기 중");
