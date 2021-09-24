8c3c845b8153eb7ab63147c50d784da237d46f4f
Chaeduk <kkj2551@naver.com>
2021-09-24 11:50:48 +0900

    edit

--- client/src/components/Post/Post.js
+++ client/src/components/Post/Post.js
@@ -17,7 +17,6 @@ const Post = ({
   firstPaging,
   lastPaging,
 }) => {
-  console.log(pageNumbers);
   return (
     <Container style={{ marginTop: 55 }}>
       <h1 style={{ marginBottom: 15 }}>Post</h1>
