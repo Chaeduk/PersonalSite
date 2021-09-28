437d2d216743600840af1555fa37af0f7746a936
Chaeduk <kkj2551@naver.com>
2021-09-28 18:14:27 +0900

    add delete

--- routes/post.js
+++ routes/post.js
@@ -45,4 +45,15 @@ router.get("/myposts/:id", async (req, res) => {
   });
 });
 
+router.delete("/:id", getUserIdByAcessToken, async (req, res) => {
+  const userId = req.userId;
+  const accessToken = req.accessToken;
+  //유저아이디와 물품 아이디 비교해 일치하는지 확인
+  await Post.remove({ _id: req.params.id });
+  res.status(200).json({
+    res: "success",
+    accessToken: accessToken,
+  });
+});
+
 module.exports = router;
